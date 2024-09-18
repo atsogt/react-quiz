917bbfd62963355a212f94318c4e30dce07aa101
atsogt <atsogt17@gmail.com>
2024-09-18 15:37:35 -0400

    useReducer for Bank App

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./components/App";
+// import "./index.css";
+// import App from "./components/App";
+import BankApp from "./bank/BankApp";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <BankApp />
   </React.StrictMode>
 );
